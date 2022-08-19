a7c38418ca8ac86193de187fd275a84a356f7173
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-08-20 01:09:49 +0200

    add functions
    hurt player when it reaches the boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,33 +7,53 @@ public class Player : MonoBehaviour
     public float speed = 1.3454f;
     private bool dead;
     [SerializeField] private int health;
+    [SerializeField] private float recoveryCounter;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello Game Dev");
+        health = 30;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
+        recoveryCounter += Time.deltaTime;
+        CheckBoundaries();
+    }
 
-        if (transform.position.x > 8.35f)
+    private void CheckBoundaries()
+    {
+        if (transform.position.x >= 8.35f)
         {
             transform.position = new Vector3(8.35f, transform.position.y, transform.position.z);
+            Hurt();
         }
-        if (transform.position.x < -8.35f)
+        if (transform.position.x <= -8.35f)
         {
             transform.position = new Vector3(-8.35f, transform.position.y, transform.position.z);
+            Hurt();
         }
-        if (transform.position.y > 4.35f)
+        if (transform.position.y >= 4.35f)
         {
             transform.position = new Vector3(transform.position.x, 4.35f, transform.position.z);
+            Hurt();
         }
-        if (transform.position.y < -4.35f)
+        if (transform.position.y <= -4.35f)
         {
             transform.position = new Vector3(transform.position.x, -4.35f, transform.position.z);
+            Hurt();
+        }
+    }
+
+    public void Hurt()
+    {
+        if (recoveryCounter > 2)
+        {
+            health -= 1;
+            recoveryCounter = 0;
         }
     }
 }
